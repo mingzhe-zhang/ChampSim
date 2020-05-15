35d7b5ce47cac3e44e215268fd2dc18ea81e23b7
mingzhe-zhang <smartzmz@hotmail.com>
2020-05-15 18:14:18 +0800

    update

--- obj/src/lpm.d
+++
@@ -1,5 +0,0 @@
-obj/src/lpm.o: src/lpm.cc inc/lpm.h inc/champsim.h
-
-inc/lpm.h:
-
-inc/champsim.h:
--- obj/src/mosaic_cache.d
+++
@@ -1,8 +0,0 @@
-obj/src/mosaic_cache.o: src/mosaic_cache.cc inc/mosaic_cache.h inc/lpm.h \
-  inc/champsim.h
-
-inc/mosaic_cache.h:
-
-inc/lpm.h:
-
-inc/champsim.h:
